23c71c96766b1c76c015b9cf9202b7b8889c24a8
Samy Pessé <samypesse@gmail.com>
2015-08-04 16:13:09 +0200

    Fix start of vertical line

--- example.js
+++ example.js
@@ -3,7 +3,6 @@ var fs = require('fs');
 
 var now = 1438441036156;
 
-
 var svg = SVGGraph([
     {
         title: 'Visits',
@@ -47,43 +46,43 @@ var svg = SVGGraph([
         color: '#1d7fb3',
         points: [
             {
-                value: 300,
+                value: 100,
                 date: now-1000
             },
             {
-                value: 100,
+                value: 1000,
+                date: now-900
+            },
+                {
+                value: 5000,
+                date: now-800
+            },
+                {
+                value: 1400,
+                date: now-700
+            },
+                {
+                value: 4500,
                 date: now-600
             },
                 {
-                value: 200,
+                value: 30,
                 date: now-500
             },
                 {
-                value: 600,
+                value: 40,
                 date: now-400
             },
                 {
-                value: 800,
+                value: 0,
                 date: now-300
-            },
-                {
-                value: 800,
-                date: now-200
-            },
-                {
-                value: 900,
-                date: now-100
-            },
-                {
-                value: 4000,
-                date: now
             }
         ]
     }
 ], {
     width: 796,
-    height: 200
+    height: 200,
+    minValue: 0
 });
 
-//console.log(svg);
-fs.writeFileSync('test.svg', svg);
+fs.writeFileSync('example.svg', svg);
